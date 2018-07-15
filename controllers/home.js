db6b9a3c2147e3f689fe0af985b971a06315aa12
Denis Asenov <denis_asenov11@abv.bg>
2018-07-15 17:39:06 +0300

    upload calc to github

---
+++ controllers/home.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+    indexGet: (req, res) => {
+        res.render('home/index');
+    },
+    indexPost: (req, res) => {
+        const Calculator = require('../models/Calculator');
+
+        let calculatorBody = req.body;
+        let calculatorParams = calculatorBody['calculator'];
+
+        let calculator = new Calculator();
+        calculator.leftOperand = Number(calculatorParams.leftOperand);
+        calculator.operator = calculatorParams.operator;
+        calculator.rightOperand = Number(calculatorParams.rightOperand);
+
+        let result = calculator.calculateResult();
+
+        res.render('home/index', {'calculator': calculator, 'result': result});
+    }
+
+};
